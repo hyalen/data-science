886b4047902db6230b829ad442d6698953969dcb
Hyalen Neves <hyalen.neves@gmail.com>
2017-06-25 19:00:55 -0300

    master

---
+++ LearningRate.java
@@ -0,0 +1,17 @@
+/**
+	 * 1st Machine Learning Concept - Training Data
+	 * @author Hyalen Neves Caldeira
+	 * @see java.MyIO
+	 * @see java.Arq
+	 * @version 1.0
+	 * @since SDK 1.8.0
+**/
+
+class TypesOfBug {
+  private int 
+}
+public class LearningRate {
+  public static void main(String [] args) {
+
+  }
+}
