8571e0c9a88002fd9ee26d5ff7462e13bd3dfbe8
Hyalen Neves <hyalen.neves@gmail.com>
2017-06-27 00:59:40 -0300

    Training Data and Sigmoid Function

---
+++ PerceptronTest.java
@@ -0,0 +1,29 @@
+/**
+	 * 1st Neural Network Concept - Multiple Perceptrons and Sigmoid Function
+	 * @author Hyalen Neves Caldeira
+	 * @version 1.0
+	 * @since SDK 1.8.0
+**/
+
+/**
+ * --This little introduction is just a trustful reminder for those (including myself) that are
+ * not 100% familiar with neural networks. Based on the book "How To Make Your Own Neural Network"--
+ *
+ * 1) What is a perceptron?
+ * A perceptron is the most basic form of an artificial neural network: a linear classifier
+ *
+ * 2) What will be tested in this code?
+ * The following concepts and core ideas taken from the book will be tested:
+ * - matrix of weights
+ * - matrix of inputs
+ * - threshold to trigger the output
+ * - sigmoid function
+ */
+class Perceptron {
+  double [][] inputs;
+  double [] weight;
+  double threshold;
+
+  public void
+
+}
