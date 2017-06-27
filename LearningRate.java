4f44c0556828d1b2c5687845d888c68960cc7c9c
Hyalen Neves <hyalen.neves@gmail.com>
2017-06-27 00:13:39 -0300

    Training Data Updated

--- LearningRate.java
+++ LearningRate.java
@@ -1,11 +1,9 @@
 /**
 	 * 1st Machine Learning Concept - Training Data
 	 * @author Hyalen Neves Caldeira
-	 * @see java.MyIO
-	 * @see java.Arq
 	 * @version 1.0
 	 * @since SDK 1.8.0
-**/ 
+**/
 
 /**
  * Based on the book 'Make Your Own Neural Network',
